ba90d08155b37dedd8484e5ee8e6fb12a159cae6
Andy Deng <andydengaroo@gmail.com>
2025-11-24 09:56:58 -0500

    More cleanup

--- constants/index.ts
+++ constants/index.ts
@@ -1,13 +1,15 @@
 import type { Scale } from '@/types';
 
+export const DEFAULT_FRAMERATE = 60;
+
 export const DEFAULT_SCALE: Scale = {
   unit: 60,
   zoom: 1 / 90,
   segments: 10
 };
 
+// Timeline left offset in pixels
 export const TIMELINE_OFFSET_X = 40;
 
 export const PREVIEW_FRAME_WIDTH = 188;
-export const DEFAULT_FRAMERATE = 60;
 export const FRAME_INTERVAL = 1000 / DEFAULT_FRAMERATE;
